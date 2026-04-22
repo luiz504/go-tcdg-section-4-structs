bf6f7d4193671328ec92db3aa3ba3f01e79f96c2
Luiz <luiz504@hotmail.com>
2026-04-22 20:23:24 -0300

    chore: exploring Zero Values

--- main.go
+++ main.go
@@ -8,18 +8,24 @@ type person struct {
 }
 
 func main() {
-
+	// very easy to fuck up if need to change the struct fields order, find some lint config to help with that.
 	// alex := person{
 	// 	"Alex",
 	// 	"Smith",
 	// }
 
-	alex := person{
-		firstName: "Alex",
-		lastName:  "Smith",
-	}
+	// alex := person{
+	// 	firstName: "Alex",
+	// 	lastName:  "Smith",
+	// }
 
-	fmt.Println(alex)         // {Alex Smith}
-	fmt.Printf("%+v\n", alex) // {firstName:Alex lastName:Smith}
+	var alex person
+	fmt.Println(alex)       // { } // Zero Value
+	fmt.Printf("%+v", alex) // {firstName:"" lastName:""}
+
+	alex.firstName = "Alex"
+	alex.lastName = "Smith"
+	fmt.Println(alex)       // {Alex Smith}
+	fmt.Printf("%+v", alex) // {firstName:"Alex" lastName:"Smith"}
 
 }
