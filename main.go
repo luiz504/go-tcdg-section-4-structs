34be3248652a03659bf688f7d44102078ce0c08d
Luiz <luiz504@hotmail.com>
2026-04-25 11:21:45 -0300

    feat: exploring pointers in go

--- main.go
+++ main.go
@@ -13,15 +13,20 @@ type person struct {
 	contactInfo
 }
 
-//	func (p person) updateFirstName(firstName string) {
-//		p.firstName = firstName
-//	}
+func (p person) tryUpdateFirstName(lastName string) {
+	p.lastName = lastName
+}
 
-func (pointerToPerson *person) updateFirstName(firstName string) {
-	(*pointerToPerson).firstName = firstName
+// The * is a  type description - it means we are working with a pointer to a person
+func (p *person) updateFirstName(firstName string) {
+	// (*arg) This is an operator - it means we want to manipulate the value the pointer is referencing
+	(*p).firstName = firstName
+	fmt.Println("updated first name", p) //updated first name &{Jimmy Smith {jim@example.com 12345}}
+	//even not using the * operator in front of the p, it prints with & symbol
 }
 
 func (p person) print() {
+
 	fmt.Printf("%+v\n", p)
 }
 
@@ -34,11 +39,17 @@ func main() {
 			zipCode: 12345,
 		},
 	}
-	jim.print() // name: Jim
-	// jim.updateFirstName("Jimmy")
 
+	jim.print() //firstName:Jim lastName:Smith contactInfo:{email:jim@example.com zipCode:12345}}
+	jim.tryUpdateFirstName("Jimmy")
+	jim.print() //firstName:Jim lastName:Smith contactInfo:{email:jim@example.com zipCode:12345}}
+
+	// the & operator is used to get the address of a variable
 	JimmyPointer := &jim
+	fmt.Println("var:", jim)              //var: {Jim Smith {jim@example.com 12345}}
+	fmt.Println("pointer:", JimmyPointer) //pointer: &{Jim Smith {jim@example.com 12345}}
 	JimmyPointer.updateFirstName("Jimmy")
 	// jim.updateFirstName("Jimmy")
-	jim.print() // name: Jimmy
+	jim.print() //{firstName:Jimmy lastName:Smith contactInfo:{email:jim@example.com zipCode:12345}}
+
 }
