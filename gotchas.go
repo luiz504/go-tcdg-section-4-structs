98e921ab3c85d088b39e9baee1659a158298feb4
Luiz <luiz504@hotmail.com>
2026-04-25 12:02:34 -0300

    feat: exploring mutations

--- gotchas.go
+++ gotchas.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func goPointerGotChas() {
+
 	mySlice := []string{"Hi", "There", "How", "Are", "You"}
 	fmt.Println("mySlice:", mySlice) //mySlice: [Hi There How Are You]
 
@@ -10,8 +11,18 @@ func goPointerGotChas() {
 	fmt.Println("mySlice:", mySlice) //mySlice: [Bye There How Are You]
 	// Even not passing a pointer it mutates the original slice~ 🤯
 
+	name := "Bill"
+	updateValue(name)
+	fmt.Println(name)   // still Bill 🤯
+	fmt.Println(&name)  // memory address of the name variable
+	fmt.Println(*&name) // the value  Bill again 🤯
+
 }
 
 func updateSlice(s []string) {
 	s[0] = "Bye"
+}
+
+func updateValue(n string) {
+	n = "Alex"
 }
