3bc5178cbd51f6cf2b712c33f8ea28948d46f8c7
Luiz <luiz504@hotmail.com>
2026-04-25 12:30:14 -0300

    feat: explorign value/reference types

--- gotchas.go
+++ gotchas.go
@@ -2,27 +2,43 @@ package main
 
 import "fmt"
 
+type customSlice []string
+
 func goPointerGotChas() {
 
-	mySlice := []string{"Hi", "There", "How", "Are", "You"}
+	mySlice := customSlice{"Hi", "There", "How", "Are", "You"}
 	fmt.Println("mySlice:", mySlice) //mySlice: [Hi There How Are You]
 
 	updateSlice(mySlice)
 	fmt.Println("mySlice:", mySlice) //mySlice: [Bye There How Are You]
 	// Even not passing a pointer it mutates the original slice~ 🤯
+	mySlice.updateByValueReceiver()
+	fmt.Println("mySlice:", mySlice) //mySlice: [By Value Receiver There How Are You]
+	// Mutate the original slice too using value receiver
+	// The slice has [length,cap,ptr to head] in memory
+	//Even passing a copy of the slice, both slices have the same [ptr to head]
 
 	name := "Bill"
-	updateValue(name)
-	fmt.Println(name)   // still Bill 🤯
+	updateString(name)
+	fmt.Println(name)   // still Bill
 	fmt.Println(&name)  // memory address of the name variable
 	fmt.Println(*&name) // the value  Bill again 🤯
+	updateStringWithPointer(&name)
+	fmt.Println(name) // Alex with pointer
 
 }
 
-func updateSlice(s []string) {
+func updateSlice(s customSlice) {
 	s[0] = "Bye"
 }
 
-func updateValue(n string) {
+func (s customSlice) updateByValueReceiver() {
+	s[0] = "By Value Receiver"
+}
+
+func updateString(n string) {
 	n = "Alex"
+}
+func updateStringWithPointer(n *string) {
+	*n = "Alex with pointer"
 }
